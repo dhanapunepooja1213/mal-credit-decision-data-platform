48fcbccb1e56cdaa6ed287d89ab2c89146e2e73d
dhanapunepooja1213 <dhanapune.pooja1213@gmail.com>
2026-07-04 11:51:25 +0530

    Create create_tables.sql

---
+++ sql/create_tables.sql
@@ -0,0 +1,21 @@
+CREATE TABLE customer_profile (
+
+customer_id VARCHAR(50),
+
+emirates_id VARCHAR(20),
+
+customer_name VARCHAR(100),
+
+email VARCHAR(100),
+
+phone VARCHAR(20),
+
+credit_score INT,
+
+fraud_score INT,
+
+aml_status VARCHAR(20),
+
+decision_timestamp TIMESTAMP
+
+);
